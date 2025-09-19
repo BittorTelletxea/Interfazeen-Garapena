aee37a341f76c3474b1b2d3177b434013067cd25
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-09-19 10:27:07 +0200

    9.Ariketa egina

--- 2.Ariketak/8.Ariketa/8.Ariketa/MainWindow.xaml.cs
+++ 2.Ariketak/8.Ariketa/8.Ariketa/MainWindow.xaml.cs
@@ -36,14 +36,47 @@ namespace _8.Ariketa
             String suma1 = Interaction.InputBox("Ingrese una fecha de la forma dd/mm/aaaa");
             String numeroMeses = Interaction.InputBox("Ingrese un numero de meses a sumar");
             
-            DateTime fecha1 = DateTime.Parse(suma1);
-            int meses = int.Parse(numeroMeses);
-            DateTime fecha2 = fecha1.AddMonths(meses);
-            suma.Text = fecha2.ToString("d");
+            
+            try
+            {
+                DateTime fecha1 = DateTime.Parse(suma1);
+                int meses = int.Parse(numeroMeses);
+                DateTime fecha2 = fecha1.AddMonths(meses);
+                suma.Text = fecha2.ToString("d");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error en el formato de la fecha");
+            }
+            String fechaInicialString = Interaction.InputBox("Ingese fecha inicial de la forma dd/mm/aaaa");
+            String fechaFinalString = Interaction.InputBox("Ingese fecha final de la forma dd/mm/aaaa");
+            try
+            {
+                DateTime fechaInicial = DateTime.Parse(fechaInicialString);
+                DateTime fechaFinal = DateTime.Parse(fechaFinalString);
+                TimeSpan diferenciaDias = fechaFinal - fechaInicial;
+                int dias = diferenciaDias.Days;
+                diferencia.Text = "Desde " + fechaInicial.ToString("d") + " hasta " + fechaFinal.ToString("d") + " hay " + dias;
+            }catch(FormatException ex)
+            {
+                MessageBox.Show("Error en el formato de la fecha");
+            }
 
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ahora.Text = "";
+            hoy.Text = "";
+            hora.Text = "";
+            suma.Text = "";
+            diferencia.Text = "";
 
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ 2.Ariketak/Ariketa9/Ariketa9/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ariketa9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            String seleccionadoAmigo = (String)lista.SelectedItem;
+            seleccionado.Text = seleccionadoAmigo;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String nuevoAmigo = nuevo.Text;
+            if(nuevoAmigo == "")
+            {
+                MessageBox.Show("Introduzca datos para poder añadirlos", "Error Añadir");
+            }
+            else
+            {
+                lista.Items.Add(nuevoAmigo);
+                nuevo.Text = "";
+            }
+               
+        }
+
+        private void eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            String seleccion = seleccionado.Text;
+            if (seleccion == "")
+            {
+                MessageBox.Show("Introduzca datos para poder eliminarlos", "Error Eliminar");
+            }
+            else
+            {
+                lista.Items.Remove(seleccion);
+                seleccionado.Text = "";
+                MessageBox.Show(seleccion + " eliminado de la lista");
+            }
+        }
+
+        private void borrar_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Items.Clear();
+        }
+
+        private void salir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
