57e45a1a22e946f26bcd8a5f6d316fdc0a852e3a
2º MULTIPLATAFORMA <IK012982I18@H012982.NET>
2025-10-01 08:54:01 +0200

    Ariketak bukatuak

---
+++ 3.Ariketak/13.Ariketa/13.Ariketa/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _13.Ariketa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEditor.SelectedText))
+                txtEditor.SelectedText = "";
+            else
+                txtEditor.Clear();
+        }
+
+        private void Arial_Click(object sender, RoutedEventArgs e)
+        {
+            txtEditor.FontFamily = new FontFamily("Arial");
+        }
+
+        private void Courier_Click(object sender, RoutedEventArgs e)
+        {
+            txtEditor.FontFamily = new FontFamily("Courier New");
+        }
+
+        private void Impact_Click(object sender, RoutedEventArgs e)
+        {
+            txtEditor.FontFamily = new FontFamily("Impact");
+        }
+
+        private void Symbol_Click(object sender, RoutedEventArgs e)
+        {
+            txtEditor.FontFamily = new FontFamily("Symbol");
+        }
+
+       
+    }
+}
